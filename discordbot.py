ba66ecc138b80903ac9a6d7ca1b0d43c98f1f2b1
오규성(데스크톱) <ogs2799@naver.com>
2024-08-29 23:51:53 +0900

    /help 기능 추가

--- discordbot.py
+++ discordbot.py
@@ -29,12 +29,19 @@ async def on_message(message):
     if message.content.startswith(f'{PREFIX}hello'):
         await message.channel.send('Hello!')
 
+    if message.content.startswith(f'{PREFIX}help'):
+        msg1 = "안녕하세요. 저는 Lol4j 전용 bot 입니다. 사용가능한 명령어는 아래와 같습니다."
+        msg2 = "/진영 : 블루/레드 중 랜덤으로 진영 선택"
+
+        await message.channel.send(msg1)
+        await message.channel.send(msg2)
+
     if message.content.startswith(f'{PREFIX}진영'):
 
         lanes = ['블루', '레드']
         index = random.randrange(0, len(lanes))
 
-        await message.channel.send(lanes[index])    
+        await message.channel.send(lanes[index])
 
 
 try:
